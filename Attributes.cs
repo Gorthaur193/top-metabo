b0ba6a1defcdd1f1183326094f4042b4b87110c2
gorthaur193 <christofer.cadavercian@ya.ru>
2023-12-13 10:54:00 +0300

    pre-finish footer

--- Attributes.cs
+++ Attributes.cs
@@ -1,10 +1,15 @@
 ﻿namespace top_metabo;
 
+/// <summary>
+/// red button by default
+/// </summary>
 public record ButtonAttributes(
-    string Background = "red", 
+    string Background = "#E4022F", 
     string BackgroundHover = "#B90011", 
     string TextColor = "white", 
-    string TextColorHover = "white");
+    string TextColorHover = "white",
+    string BorderColor = "#E4022F",
+    string BorderColorHover = "#B90011");
 
 public record TextboxAttributes(
     string Height = "",
